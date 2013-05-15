d793f51c03563a4297aa907aefd34f4b640953fe
Bernd Ahlers <bernd@tuneafish.de>
2013-05-15 20:51:42 +0200

    Configure logging and change hz instance creation.
    - Use full qualified class name for Config to avoid RbConfig warnings.
    - Use Hazelcast.new_hazelcast_instance to create a new instance instead
      of spawning the default one.
    - Add custom log listener that forwards log messages to the dcell
      logger.
    - Disable hazelcast system logging and set logging type to 'none' to
      avoid spamming the console.

--- lib/dcell/hazelcast.rb
+++ lib/dcell/hazelcast.rb
@@ -3,18 +3,38 @@ require 'forwardable'
 
 module DCell
   class Hazelcast
-    import 'com.hazelcast.core.Hazelcast'
-    import 'com.hazelcast.config.Config'
+    class LogListener
+      def log(event)
+        DCell::Logger.info(event.get_log_record.get_message)
+      end
+    end
+
+    java_import 'com.hazelcast.core.Hazelcast'
+    java_import 'java.util.logging.Level'
+    java_import 'java.lang.System'
 
     def self.shutdown_all
       Hazelcast.shutdown_all
     end
 
     def initialize
-      cfg = Config.new
-      cfg.set_property('hazelcast.version.check.enabled', 'false')
+      # Disable default logging to avoid messages to STDOUT.
+      System.set_property('hazelcast.logging.type', 'none')
 
-      @hazelcast = Hazelcast.init(cfg)
+      cfg = com.hazelcast.config.Config.new
+      cfg.set_property('hazelcast.version.check.enabled', 'false')
+      cfg.set_property('hazelcast.memcache.enabled', 'false')
+      cfg.set_property('hazelcast.rest.enabled', 'false')
+
+      # Disable system log to avoid messages to STDOUT.
+      cfg.set_property('hazelcast.system.log.enabled', 'false')
+
+      # Set hazelcast group name to avoid hazelcast instances joining other
+      # existing groups.
+      cfg.get_group_config.set_name('hz-dcell')
+
+      @hazelcast = Hazelcast.new_hazelcast_instance(cfg)
+      @hazelcast.get_logging_service.add_log_listener(Level::INFO, LogListener.new)
     end
 
     def get_map(name)
